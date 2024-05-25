9e1c2ee32444a93abc0e398807740c7fa2afec14
Mariia <mariia.garkavenko@creativelift.ai>
2024-05-25 21:28:47 +0200

    Dashboard for chatbot evaluation

--- app/chatbot_evaluator.py
+++ app/chatbot_evaluator.py
@@ -3,12 +3,10 @@ import pandas as pd
 from helpers import mistral
 
 
-
-
 def create_metrics_schema(metrics_text):
     # Split the input string by commas to get individual metrics
     metrics = [metric.strip() for metric in metrics_text.split(',')]
-    
+
     # Initialize the schema dictionary
     schema = {
         "type": "object",
@@ -20,19 +18,19 @@ def create_metrics_schema(metrics_text):
     for metric in metrics:
         schema["properties"][metric] = {"type": "float"}
         schema["required"].append(metric)
-    
-    
+
     return schema
 
-def evaluate_chatbot(combined_dialogues, metrics_text):
-    results=[]
-    metrics_json_schema = create_metrics_schema(metrics_text)
 
+def evaluate_chatbot(combined_dialogues, metrics_text):
+    results = []
     metrics_text = """Understanding User Queries, Providing Relevant Information, User Interaction and Engagement, 
     Personalization and Context Awareness, Overall Satisfaction"""
+    metrics_json_schema = create_metrics_schema(metrics_text)
 
-    for i in range(1):
-        chat=combined_dialogues.iloc[i,1]
+
+    for i in range(1, 4):
+        chat = combined_dialogues.iloc[i, 1]
         prompt = f"""
         You are a quality assurance specialist evaluating AI assistants. 
         Your goal is to assess the user's level of satisfaction with the chat based on the following metrics:
@@ -43,16 +41,10 @@ def evaluate_chatbot(combined_dialogues, metrics_text):
 
         {metrics_json_schema}
         """
-        print(prompt)
-        response = mistral(prompt, model= 'mistral-large-latest', is_json=True)
+        response = {'Dialogue': chat}
+        response.update(eval(mistral(prompt, model='mistral-large-latest', is_json=True)))
         results.append(response)
-    
-    results_df=pd.DataFrame([eval(i) for i in results])
+
+    results_df = pd.DataFrame(results)
 
     return results_df
-
-
-
-
-
-
--- app/main.py
+++ app/main.py
@@ -4,8 +4,17 @@ import streamlit as st
 import pandas as pd 
 from evaluator import MistralEvaluator
 from chatbot_evaluator import *
+import altair as alt
+
 
 def main():
+    st.set_page_config(
+        page_title="💡 Genies - LLM Quality Analysis ",
+        page_icon="💡",
+        layout="wide",
+        initial_sidebar_state="expanded")
+    alt.themes.enable("dark")
+
     st.markdown("# 💡 Genies - LLM Quality Analysis ")
     st.markdown("### Project Description:")
     project_description = st.text_input("📝 Please provide a brief description of your LLm Based application:")
@@ -28,7 +37,29 @@ def main():
             
             # Display the results in Streamlit
             st.markdown("### Evaluation Results")
-            st.dataframe(results.head(5))
+            col = st.columns((4.5, 4.5), gap='medium')
+            example_idx = 2
+            with col[0]:
+                st.markdown('#### Example')
+                example = '###**Dialogue**###\n\n' + results.iloc[example_idx]['Dialogue'].replace(
+                    '\n', '\n\n').replace('Human:', '**Human:**').replace(
+                    'Travel Assistant:', '**Travel Assistant:**') + '\n\n###**Comment**###'
+                score = results.iloc[example_idx]['Overall Satisfaction']
+                comment = results.iloc[example_idx]['Comment']
+                example += f'**[{score}/5]** {comment}\n\n'
+                st.write(example)
+            with col[1]:
+                st.markdown('#### Evaluation Scores on the dataset')
+                st.metric(label="Overall Satisfaction", value=round(results['Overall Satisfaction'].mean(), 1))
+                st.markdown("""---""")
+                st.metric(label="Understanding User Queries", value=round(results['Understanding User Queries'].mean(), 1))
+                st.metric(label="Providing Relevant Information",
+                          value=round(results['Providing Relevant Information'].mean(), 1))
+                st.metric(label="User Interaction and Engagement",
+                          value=round(results['User Interaction and Engagement'].mean(), 1))
+                st.metric(label="Personalization and Context Awareness",
+                          value=round(results['Personalization and Context Awareness'].mean(), 1))
+
 
 if __name__ == "__main__":
     main()
