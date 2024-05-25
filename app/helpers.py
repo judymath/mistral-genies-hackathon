5f4946f66b61349601ed50609306101b4ad6eb06
Siwar ABBES <siwar.abbes@green-acres.com>
2024-05-25 17:04:15 +0200

    commits

--- app/helpers.py
+++ app/helpers.py
@@ -2,12 +2,24 @@ import os
 from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+# Define BASEDIR for the Project
+BASEDIR = Path(__file__).parents[1]
+
+# Load environment variables locally
+if os.path.isfile(os.path.join(BASEDIR, ".env")):
+    load_dotenv(os.path.join(BASEDIR, ".env"))
+
+
 
 def mistral(user_message,
             model,
             is_json=False):
 
-    client = MistralClient(api_key=os.environ["MISTRAL_API_KEY"])
+    client = MistralClient(api_key=os.environ.get("MISTRAL_API_KEY", None))
     messages = [ChatMessage(role="user", content=user_message)]
     if is_json:
         chat_response = client.chat(
--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
+#description = 'a travel assistant that search hotels according to the criteria provided by the user'
 
+import streamlit as st
+from evaluator import MistralEvaluator
 
-
+def main():
+    st.markdown("# 💡 Genies - LLM Quality Analysis ")
+    st.markdown("### Project Description:")
+    project_description = st.text_input("📝 Please provide a brief description of your LLm Based application:")
+    
+    if project_description:
+        mistral_evaluator = MistralEvaluator(project_description)
+        st.markdown("### 📊 Generated Metrics")
+        st.markdown("Based on your description, our evaluator suggests the following metrics. Which ones would you like to use?")
+        # todo : replace options by generated metrics
+        options = mistral_evaluator.generate_metrics()
+        
+        selected_metrics = []
+        for option in options:
+            if st.checkbox(option):
+                selected_metrics.append(option)
+        
 
 if __name__ == "__main__":
-    description = 'a travel assistant that search hotels according to the criteria provided by the user'
+    main()
